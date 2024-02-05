b9fd64dd645c53dd8507f0447efae063de6dd35c
Nathan Levitt <nathan@trudine.com>
2024-02-05 15:42:42 -0800

    feat: shadcn + dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <h1 className="text-sm font-medium">Nate&apos;s Next.js Starter</h1>
+    <div className="flex flex-col space-y-2.5 items-center justify-center w-screen h-screen">
+      <h1 className="font-medium">Nate&apos;s Next.js Starter</h1>
+      <Button className="text-xs">Get Started</Button>
     </div>
   );
 }
