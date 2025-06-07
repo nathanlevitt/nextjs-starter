f46e80ceeed2c9b29331ae3048bfb69a9a619994
Nathan Levitt <nathan@trudine.com>
2025-06-07 09:50:55 -0700

    feat: db camel case plugin

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,5 +1,8 @@
 import { createKysely } from "@vercel/postgres-kysely";
+import { CamelCasePlugin } from "kysely";
 
 import { Database } from "./schema";
 
-export const db = createKysely<Database>();
+export const db = createKysely<Database>(undefined, {
+  plugins: [new CamelCasePlugin()],
+});
